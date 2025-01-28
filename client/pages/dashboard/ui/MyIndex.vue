380ad06146f185785248c0d2fc114e9c30553571
AwesomeXjs <bestawes0mex@gmail.com>
2025-01-28 12:55:53 +0500

    try create portfolio with auth token

--- client/pages/dashboard/ui/MyIndex.vue
+++ client/pages/dashboard/ui/MyIndex.vue
@@ -35,10 +35,13 @@ const tryValidate = async () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `TGWebApp ${authToken}`
+        'TGWebAppToken': `TGWebApp ${authToken}`,
       },
-      body: JSON.stringify({}), // Пустое тело, если необходимо
-      baseURL: 'http://127.0.0.1:8080',
+      body: JSON.stringify({
+        "name": "Shit coins",
+        "owner_id": 518774723
+      }),
+      baseURL: 'http://127.0.0.1:8080/api/v1/create-portfolio',
     });
 
     console.log("response: ", response);
